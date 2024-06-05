1b95d16dfcadb3c166411e8508dc57c8df0d602e
Emma <emmaminary@gmail.com>
2024-06-05 15:45:23 +0200

    form add new recipe

---
+++ migrations/Version20240605115719.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240605115719 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE email RENAME INDEX email TO UNIQ_E7927C74E7927C74');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE add_recipe');
+        $this->addSql('ALTER TABLE email RENAME INDEX uniq_e7927c74e7927c74 TO email');
+    }
+}
---
+++ src/Controller/AddRecipeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Form\AddRecipeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AddRecipeController extends AbstractController
+{
+    #[Route('/add/recipe', name: 'app_add_recipe')]
+    public function addrecipe(Request $request, EntityManagerInterface $em): Response
+    {
+        $addRecipe = new Recipe();
+        $form = $this->createForm(AddRecipeType::class, $addRecipe); // Pour créer le formulaire
+
+        $form->handleRequest(request: $request);
+
+        if ($form->isSubmitted())   //Vérification de la validité du formulaire
+        { 
+            $em->persist($addRecipe);
+            $em->flush();
+            return $this->redirectToRoute('addok');
+        }
+
+        return $this->render('add_recipe/addrecipe.html.twig', [
+            'recipeForm' => $form
+        ]);
+    }
+
+    #[Route('/add_recipe/addok', name: 'addok')]
+    public function addok(): Response
+    {
+        return $this->render('add_recipe/addokrecipe.html.twig');
+    }
+}
